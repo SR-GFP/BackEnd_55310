43ad39bf89ac3f8cc30da0a356e2be5140417530
Gonzalo Pompei <gonzalopompei@gmail.com>
2023-07-15 17:58:52 -0300

    addprudcts: el metodo busca si existe el id, guarda y muestra por consola, correctamente

--- Entregas/BackEnd_desafio2_Pompei/Classes/ProductManager.js
+++ Entregas/BackEnd_desafio2_Pompei/Classes/ProductManager.js
@@ -2,8 +2,46 @@ const fs = require("fs")
 
 class ProductManager{
   constructor (file){
-    this.path = `${process.cwd()}/Files/${file}`
+    this.path = `${process.cwd()}/Files/${file}`;
+    this.products = [];
+    this.lastID = 0;
   }
   /*Metodos */
+  addProducts (title, description, price, thumbnail, code, stock){
+    try {
+      const codeExist = this.products.find((product)=> product.code === code)
+    if (codeExist){
+      const mensaje = console.log("El codigo ya existe");
+      return mensaje;
+    }else{
+      const ID = this.lastID += 1;
+      const newProduct = {
+        ID,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      }
+      this.products.push(newProduct);
+      const mensaje = console.log(`Producto Agregado correctamente el ID del producto es: ${newProduct.ID}`);
+        return mensaje;   
+    }    
+      
+    } catch (error) {
+      
+    }
+    
+  }
+  
   
 }
+
+const productManager = new ProductManager();
+productManager.addProducts("Cocacola", "Gaseosa gusto Cola", 400, "sin Imagen", "COKE1", 200)
+productManager.addProducts("Cocacola", "Gaseosa gusto Cola", 400, "sin Imagen", "COKE2", 200)
+
+
+
+
