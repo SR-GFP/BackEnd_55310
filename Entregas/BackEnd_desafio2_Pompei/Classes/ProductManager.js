ab3b998b1459b8851ee6c81a9ca09c74546d3175
Gonzalo Pompei <gonzalopompei@gmail.com>
2023-07-15 21:36:57 -0300

    Commit: Se cambia ruta /files/ por que da error, con ../Files/ funciona bien y se crea el archivo

--- Entregas/BackEnd_desafio2_Pompei/Classes/ProductManager.js
+++ Entregas/BackEnd_desafio2_Pompei/Classes/ProductManager.js
@@ -1,18 +1,19 @@
-const fs = require("fs")
+const fs = require("fs");
+const { json } = require("stream/consumers");
 
 class ProductManager{
   constructor (file){
-    this.path = `${process.cwd()}/Files/${file}`;
+    this.path = `../Files/${file}`;
     this.products = [];
     this.lastID = 0;
   }
   /*Metodos */
   addProducts (title, description, price, thumbnail, code, stock){
-    try {
+    try {      
       const codeExist = this.products.find((product)=> product.code === code)
-    if (codeExist){
-      const mensaje = console.log("El codigo ya existe");
-      return mensaje;
+      if (codeExist){
+        const mensaje = console.log("El codigo ya existe");
+        return mensaje;
     }else{
       const ID = this.lastID += 1;
       const newProduct = {
@@ -23,24 +24,29 @@ class ProductManager{
         thumbnail,
         code,
         stock,
-      }
+      };
       this.products.push(newProduct);
-      const mensaje = console.log(`Producto Agregado correctamente el ID del producto es: ${newProduct.ID}`);
-        return mensaje;   
-    }    
-      
-    } catch (error) {
-      
+      this.saveProductsFile();
+      const mensaje = console.log(`Producto Agregado correctamente el ID del producto es: ${this.products}`);
+      return mensaje;
     }
-    
+    } catch (error) {
+      const mensaje = console.log(error);
+      return mensaje;        
+    }    
   }
-  
-  
+  async saveProductsFile(){
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products))      
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
-const productManager = new ProductManager();
-productManager.addProducts("Cocacola", "Gaseosa gusto Cola", 400, "sin Imagen", "COKE1", 200)
-productManager.addProducts("Cocacola", "Gaseosa gusto Cola", 400, "sin Imagen", "COKE2", 200)
+const productManager = new ProductManager("ListaDeProductos");
+productManager.addProducts("falopa", "energizante", 600, "sin Imagen", "Jorgito triple blanco", 2)
+productManager.addProducts("Cocacola", "Gaseosa gusto Cola", 400, "sin Imagen", "Jorgito triple blanco", 200)
 
 
 
