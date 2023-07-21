29d59bc88666183bc7216ab79db502fa92ce0fd7
SR-GFP <gonzalopompei@gmail.com>
2023-07-21 17:42:25 -0300

    Commit: Server inicializado

--- Entregas/BackEnd_desafio3_Pompei/index.js
+++ Entregas/BackEnd_desafio3_Pompei/index.js
@@ -1,2 +1,11 @@
 const express = require ("express");
+const ProductManager = require ("./ProductManager");
+const app = express();
+
+const productManager = new ProductManager ();
+
+const puerto = 3000;
+app.listen(puerto,()=>{
+    console.log(`Server cooriendo en http://localhost:${puerto}`)
+})
 
