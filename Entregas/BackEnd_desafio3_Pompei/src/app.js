65fd4072c4aee5ce9c26fcf7ac1c4705309f7186
Gonzalo Pompei <gonzalopompei@gmail.com>
2023-07-24 00:37:49 -0300

    Commit: Se teste siguiendo la rubrica, server funcionando correctamente

--- Entregas/BackEnd_desafio3_Pompei/src/app.js
+++ Entregas/BackEnd_desafio3_Pompei/src/app.js
@@ -5,9 +5,11 @@ const app = express();
 const productManager = new ProductManager("Products");
 
 
+// Endpoint para obtener todos los productos con posibilidad de limitar el resultado
+// Si no se proporciona el parámetro 'limit', devuelve todos los productos; de lo contrario, devuelve los productos limitados según el valor de 'limit'
 app.get("/products", async (req, res)=>{
   try {    
-    const {limit} = req.query || null;
+    const {limit} = req.query || null; // Obtiene el parámetro 'limit' desde el query de la URL
     const products = await productManager.getProducts();
     const limitedProducts = products.slice(0, limit);
     return !limit ? res.json(products) : res.json(limitedProducts);        
@@ -16,9 +18,11 @@ app.get("/products", async (req, res)=>{
   }
 })
 
+
+// Endpoint para obtener un producto por su id (pid)
 app.get("/products/:pid", async (req, res)=>{
   try {
-    const {pid} = req.params;
+    const {pid} = req.params; // Obtiene el parámetro 'pid' desde los parámetros de la URL
     const product = await productManager.getProductById(Number(pid));
     res.json(product);    
   } catch (error) {
