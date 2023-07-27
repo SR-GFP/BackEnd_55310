b1f73218572a10b3280428097146f383bb00a878
SR-GFP <gonzalopompei@gmail.com>
2023-07-27 17:34:47 -0300

    Commir: creando la carpeta routes

--- Entregas/BackEnd_Preentrega1_Pompei/src/app.js
+++ Entregas/BackEnd_Preentrega1_Pompei/src/app.js
@@ -1,4 +1,11 @@
 const express = require ("express");
-const producManager = require ("./ProductManager");
+const ProductManager = require ("./ProductManager");
+const productManager = ProductManager;
 const puerto = 8080;
-  
+const app = express();
+app.use(express.static("public"))
+
+
+app.listen(puerto, ()=>{
+  console.log(`Server corriendo en localhost:${puerto}`)
+})
